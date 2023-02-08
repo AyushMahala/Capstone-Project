e9fda8bfbeb713db8287212139dd025e8624d0b8
AyushMahala <81615691+AyushMahala@users.noreply.github.com>
2023-02-08 16:08:15 +0530

    Final Commit

--- src/components/Todoform.js
+++ src/components/Todoform.js
@@ -1,7 +1,7 @@
 import React,{useState,useEffect,useRef} from 'react';
 
 function Todoform(props) {
-    const  [input,setInput] = useState('');
+    const  [input,setInput] = useState(props.edit ? props.edit.value : "");
 
     const inputRef = useRef(null);
 
@@ -26,8 +26,24 @@ function Todoform(props) {
 
   return (
     <form  className="todo-form" onSubmit={handleSubmit}>
+      {props.edit ? (
+        <>
+                <input ref={inputRef}
+                 type="text"
+                  onChange={handleChange}
+                   placeholder='Update Your Item'
+                    value={input}
+                     name="text" 
+                     className='todo-input edit' />
+                <button  className='todo-button edit'>Update</button>
+                </>
+      ):(
+        <>
         <input ref={inputRef} type="text" onChange={handleChange} placeholder='Add a todo' value={input} name="text" className='todo-input' />
         <button  className='todo-button'>Add Todo</button>
+        </>
+)}
+
     </form> 
   );
 }
